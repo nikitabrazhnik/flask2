1269346f4acf437474cc273d283cd9ac0b35033e
GoodBytes <nikita.brazhnik@gmail.com>
2017-07-22 19:20:47 +0300

    new

--- hello.py
+++ hello.py
@@ -13,10 +13,5 @@ def index_i():
     return "Hello, World 2!"
 
 
-<<<<<<< HEAD
-#edde
-=======
-#e
->>>>>>> 93dc51fb1113d06c17cc894d72964cec3eefba66
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
