7729e20fb01e894aa1701b97da87fb4acf66916b
GoodBytes <nikita.brazhnik@gmail.com>
2017-07-22 19:01:29 +0300

    new

---
+++ hello.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello, World!"
+
+
+@app.route("/i")
+def index_i():
+    return "Hello, World 2!"
+
+
+#edd
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
